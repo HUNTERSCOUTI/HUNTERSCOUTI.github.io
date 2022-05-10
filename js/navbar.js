0b2a6d5a21fd2e5078889d38a3d30a9e1ad691b2
Zilas Jørgensen <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-05-10 03:42:00 +0200

    Delete js directory

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {myFunction()};
-            
-    var header = document.getElementById("siteNav");
-    var sticky = header.offsetTop;
-            
-    function myFunction() {
-        header.classList.add("sticky");
-        document.getElementById("siteNav").setAttribute("style","height:50px");
-}
